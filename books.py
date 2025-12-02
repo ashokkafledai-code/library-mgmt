0f430ad099491a5fa673c081c0367d99fe88dcfb
ashokkafledai-code <ashok.kafledai@gmail.com>
2025-12-02 13:02:24 +0545

    code refactor

--- books.py
+++ books.py
@@ -38,29 +38,3 @@ def return_book():
     else:
         print("Book is already available.\n")
 
-def main():
-    while True:
-        print("===== Library Menu =====")
-        print("1. Add Book")
-        print("2. View Books")
-        print("3. Borrow Book")
-        print("4. Return Book")
-        print("5. Exit")
-        
-        choice = input("Choose an option: ")
-
-        if choice == "1":
-            add_book()
-        elif choice == "2":
-            view_books()
-        elif choice == "3":
-            borrow_book()
-        elif choice == "4":
-            return_book()
-        elif choice == "5":
-            print("Goodbye!")
-            break
-        else:
-            print("Invalid option.\n")
-
-main()
